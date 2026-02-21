dd3febdb2cb72c539e628a9c0413b22b9abd13af
Zacinet <zacinet@gmail.com>
2026-02-20 20:46:09 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,1 @@
+export default function Home() { return ( <main style={{ padding: "40px", fontFamily: "Arial" }}> <h1>Bienvenido a Zacinet ERP</h1> <p>Sistema profesional en construcción.</p> </main> ); }
